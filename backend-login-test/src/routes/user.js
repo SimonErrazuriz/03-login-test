e2da541223d62f02227f555ee630cc1d3e0cdf59
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-11-26 22:34:47 -0300

    feat: crear ruta para el ingreso del usuario

--- backend-login-test/src/routes/user.js
+++ backend-login-test/src/routes/user.js
@@ -9,10 +9,22 @@ router.post('/registro', async (req, res) => {
     const { email, password } = req.body;
     const newUser = new User({ email, password });
     await newUser.save();
-    
+
     /* Token */
-    const token = jwt.sign({_id: newUser._id}, 'secretKey');
-    res.status(200).json({token});
+    const token = jwt.sign({ _id: newUser._id }, 'secretKey');
+    res.status(200).json({ token });
+});
+
+router.post('/ingreso', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send('email no encontrado');
+    if(user.password !== password) return res.status(401).send('contraseña incorrecta');
+
+    const token = jwt.sign({_id: user.id}, 'secretKey');
+    return res.status(200).json({token});
+
 });
 
 
