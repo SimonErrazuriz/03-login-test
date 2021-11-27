934200c86daea5ffeb925adf7f3463e98eba350c
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-11-26 21:24:19 -0300

    feat: crear ruta para el registro del usuario

--- backend-login-test/src/index.js
+++ backend-login-test/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 require('./database');
 
-app.use(require('./routes/user'));
+app.use(express.json());
+app.use('/api', require('./routes/user'));
 
-app.listen(3000);
-console.log('Servidor en el puerto: ', port);
+app.listen(port, () => {
+    console.log(`Servidor en el puerto http://localhost:${port}/api`)
+});
+
--- backend-login-test/src/routes/user.js
+++ backend-login-test/src/routes/user.js
@@ -1,6 +1,16 @@
 const { Router } = require('express');
+const User = require('../models/User');
+
 const router = Router();
 
-router.get('/', (req, res) => res.send('Holaaa'));
+router.get('/', (req, res) => res.send('Respuesta para probar ruta'));
+
+router.post('/registro', async (req, res) => {
+    const { email, password } = req.body;
+    const newUser = new User({ email, password });
+    await newUser.save();
+    res.send('Registrado')
+});
+
 
 module.exports = router;
